8cee423bcfd47c6789ec679f9b9ddd4870849b8b
git-your-hub <arischild1@gmail.com>
2018-11-26 20:40:37 -0500

    program looks nice and neat now, easy to understand

--- crook.cpp
+++ crook.cpp
@@ -1,109 +1,126 @@
-#include <cstdlib>
+
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <unistd.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <stdio.h>
 #include <vector>
-#include <algorithm>
-#include <queue>
 #include <ctime>
 #include <tgmath.h>
 
 using namespace std;
 
-vector<vector <int> > simpleSolve(vector<vector<int> > grid, int n);
-bool valid(vector<vector <int> > grid, int n);
-string gridToString(vector<vector<int> > grid, int n);
-int countBlanks(vector<vector <int> > grid, int n);
+bool valid(vector<vector <int> > grid, int i, int j);
+string gridToString(vector<vector<int> > grid);
+int countBlanks(vector<vector <int> > grid);
+vector<vector <int> > simpleSolve(vector<vector<int> > grid);
+vector<vector <int> > crook(vector<vector<int> > grid);
 
+// the n x n size of the grid
+int n;
+// the number of blanks remaining in the grid
 int blanks;
-int lowestBlank = 140;
 
 /*
- * input format: first line contains the value of n for the n x n the grid, followed by the sudoku itself,
- * with each value separated by a space or new line. blank spaces are represented as 0.
- * Example input:
+ * Input file format: first line contains the value of n for the n x n the grid, followed by the sudoku itself,
+ * with each value separated by a space, and each row seperated by a new line. blank spaces are represented as 0.
+ * Example input file:
  * 4
  * 0 2 0 4
  * 4 1 2 0
  * 0 4 0 0
  * 0 0 4 0
  *
- * multiple sudoku puzzles can be solved with one input file, seperated by a new line, each
- * with its own value of n.
  *
- * Acceptable values of n must have a natural number for a square root. Program assumes 
- * proper input.
+ * Acceptable values of n must have a natural number square root. Program assumes 
+ * proper input. For now only 4x4's and 9x9's work properly. 16x16's work theoretically, 
+ * but take too long in execution to actually test.
+ *
+ * Command line format: ./crook <input file name> <algorithm name>
+ *  <algorithm name> is either "simple" or "crook's"
+ *
+ * Example command line: ./crook sudoku.txt simple
  */
+
 
 int main(int argc, char **argv) {
 	string infile = argv[1];
-	string outfile = argv[2];
+	string alg = argv[2];
 	ifstream IN(infile);
-	ofstream OUT(outfile);
 	string line;
-	while (IN.is_open() && getline(IN, line)) {
-		if (line.empty()) break;
+	if (IN.is_open() && getline(IN, line)) {
 		stringstream ss;
 		ss << line;
 		string temp;
 		ss >> temp;
-		int n = stoi(temp);
+		n = stoi(temp);
 		vector<vector<int> > grid;
 		for(int i = 0; i < n; i++) {
 			vector<int> row;
-			stringstream ss2;
-			string temp1;
 			getline(IN, line);
-			ss2 << line;
+			ss.clear();
+			ss << line;
 			for (int j = 0; j < n; j++) {
-				ss2 >> temp1;
-				row.push_back(stoi(temp1));
+				ss >> temp;
+				row.push_back(stoi(temp));
 			}
 			grid.push_back(row);
 		}
 		clock_t begin = clock();
-		blanks = countBlanks(grid, n);
-		grid = simpleSolve(grid, n);
-		cout << gridToString(grid, n);
+		blanks = countBlanks(grid);
+	    if(alg == "simple") grid = simpleSolve(grid);
+		if(alg == "crook's") grid = crook(grid);
+		cout << gridToString(grid);
 		clock_t end = clock();
 		double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 		cout << "Time to solve: " << to_string(elapsed_secs) << endl;
 	}
-	OUT.close();
 	IN.close();
 }
 
-bool valid(vector<vector <int> > grid, int n) {
+/*
+ * determines if a grid violates rules of a sudoku puzzle after updating a value
+ * will return false if there is a duplicate number in the given row, column, or box.
+ */ 
+
+bool valid(vector<vector <int> > grid, int i, int j) {
 	int root = sqrt(n);
-	for (int i = 0; i < n; i++) {
-		for(int j = 0; j < n; j++) {
-			if (grid[i][j] != 0) {
-				int val = grid[i][j];
-				for (int k = 0; k < n; k++) {
-					if (grid[i][k] == val && k != j) return false;
-					if (grid[k][j] == val && k != i) return false;
-				}
-				int boxRowStart = i/root * root;
-				int boxColStart = j/root * root;
-				for (int k = boxRowStart; k < boxRowStart + root; k++) {
-					for (int h = boxColStart; h < boxColStart + root; h++) {
-						if (grid[k][h] == val && !(k == i && h == j)) {
-							return false;
-						}
-					}
-				}
+	int val = grid[i][j];
+	for (int k = 0; k < n; k++) {
+		if (grid[i][k] == val && k != j) return false; // check each value in the row
+		if (grid[k][j] == val && k != i) return false; // check each value in the column
+	}
+	int boxRowStart = i/root * root; // find first row in box
+	int boxColStart = j/root * root; // find first row in column
+	for (int k = boxRowStart; k < boxRowStart + root; k++) {
+		for (int h = boxColStart; h < boxColStart + root; h++) {
+			if (grid[k][h] == val && !(k == i && h == j)) { // check each value in box
+				return false;
 			}
 		}
 	}
 	return true;
 }
 
-int countBlanks(vector<vector <int> > grid, int n) {
+/*
+ * converts the grid to a string for easy printing
+ */
+
+string gridToString(vector<vector< int> > grid) {
+	string retval = "";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			retval += to_string(grid[i][j]) + " ";
+		}
+	retval += '\n';
+	}
+	return retval;
+}
+
+/*
+ * used to initialize the value of blanks
+ */
+
+int countBlanks(vector<vector <int> > grid) {
 	int blanks = 0;
 	for (int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++) {
@@ -113,19 +130,21 @@ int countBlanks(vector<vector <int> > grid, int n) {
 	return blanks;
 }
 
-vector<vector <int> > simpleSolve(vector<vector<int> > grid, int n) {
+/*
+ * simple brute force algorithm. recursively tries every possible combination
+ */
+
+vector<vector <int> > simpleSolve(vector<vector<int> > grid) {
 	if (blanks == 0) return grid;
 	for (int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++) {
-			if (grid[i][j] == 0) {
+			if (grid[i][j] == 0) { 
 				for (int k = 1; k <= n; k++) {
 					grid[i][j] = k;
-					if(valid(grid,n)) {
+					if(valid(grid, i, j)) {
 						blanks--;
-						if (blanks < lowestBlank) lowestBlank = blanks;
-						cout << to_string(blanks) << " " << to_string(lowestBlank) << endl;
 						int before = blanks;
-						grid = simpleSolve(grid,n);
+						grid = simpleSolve(grid);
 						if (blanks != before || blanks == 0) return grid;
 						else blanks++;
 						if (k == n) {
@@ -143,13 +162,11 @@ vector<vector <int> > simpleSolve(vector<vector<int> > grid, int n) {
 	return grid;
 }
 
-string gridToString(vector<vector< int> > grid, int n) {
-	string retval = "";
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			retval += to_string(grid[i][j]) + " ";
-		}
-	retval += '\n';
-	}
-	return retval;
+/*
+ * the big bad wolf
+ */
+
+vector<vector <int> > crook(vector<vector<int> > grid) {
+	// this is where crook's algorithm goes
+	return grid;
 }
